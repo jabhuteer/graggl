9be796083c4000ebb8524d06ab121f52f7b8243e
Jabhuteer <jabhuteer1@gmail.com>
2024-06-27 09:53:23 -0500

    Added users

--- src/Data/users.js
+++ src/Data/users.js
@@ -12,5 +12,59 @@ export default
         },
         {
             "name": "Kelli"
-        }
+        },
+        {
+            "name": "Bared"
+        },
+        {
+            "name": "Boss"
+        },
+        {
+            "name": "Liz"
+        },
+        {
+            "name": "Vetrix"
+        },
+        {
+            "name": "Riley"
+        },
+        {
+            "name": "Rose"
+        },
+        {
+            "name": "Corvus"
+        },
+        {
+            "name": "Doot"
+        },
+        {
+            "name": "Twi"
+        },
+        {
+            "name": "Axa"
+        },
+        {
+            "name": "Strata"
+        },
+        {
+            "name": "Garfield"
+        },
+        {
+            "name": "Cheez"
+        },
+        {
+            "name": "Rin"
+        },
+        {
+            "name": "Abdur"
+        },
+        {
+            "name": "NTO"
+        },
+        {
+            "name": "Ren"
+        },
+        {
+            "name": "Nancy"
+        },
     ]
